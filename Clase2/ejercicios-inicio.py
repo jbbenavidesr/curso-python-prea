cb32c2191f55e4cb86104fc787625203c4cd3ca4
Juan Bernardo Benavides <jbbenavidesr@unal.edu.co>
2021-07-31 10:08:33 -0500

    Add exercise

---
+++ Clase2/ejercicios-inicio.py
@@ -0,0 +1,50 @@
+# Para empezar el día de hoy, hagamos un pequeño ejercicio con lo que vimos la
+# clase pasada.
+
+# 1. Imprimir en la consola la frase:
+# Juan le dijo que "si pretendo llegar a tiempo, debo salir ya"
+mensaje = 'Juan le dijo que "si pretendo llegar a tiempo, debo salir ya" '
+print(mensaje)
+
+
+# 2. Imprimir la frase:
+# I'm very proud of Joseph's achievements
+mensaje_2 = "I´m very proud of Joseph's achievements"
+print(mensaje_2)
+
+
+
+# 3. Imprimir el siguiente parrafo
+mensaje_3 = "This planet has-or rather had-a problem, which  was this: most of the people\
+living on it were unhappy for pretty much of the time. Many solutions\
+were suggested for this problem, but most of these were largely concened\
+with the movements of small green pieces of paper, which is odd \
+because on the whole it wasn't the small green pieces of paper that were,\
+unhappy."
+print(mensaje_3)
+
+
+
+
+
+# 4. Imprimir el siguiente poema:
+mensaje_4 = "Mi árbol tenía\n\
+sus ramas de oro.\n\
+Un viento envidioso\n\
+robó mi tesoro.\n\
+Hoy no tiene ramas.\n\
+Hoy no tiene sueños\n\
+mi árbol callado,\n\
+mi árbol pequeño."
+print(mensaje_4)
+
+mensaje_5 = """Mi árbol tenía
+sus ramas de oro.
+Un viento envidioso
+robó mi tesoro.
+Hoy no tiene ramas.
+Hoy no tiene sueños
+mi árbol callado,
+mi árbol pequeño."""
+
+print(mensaje_5)
